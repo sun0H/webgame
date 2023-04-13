11164a230989523e2b1a341b45de75c8717c4812
sun0H <108638572+sun0H@users.noreply.github.com>
2023-04-13 14:43:39 +0900

    로또(초기)

---
+++ 6.로또/LottoGenerator.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+
+      }
+    },
+    computed:{
+
+    },
+    methods: {
+
+    },
+
+  };
+</script>
+
+<style scoped>
+
+
+</style>
