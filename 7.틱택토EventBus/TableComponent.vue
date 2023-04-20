6c31166c46fe3b067961c15c9ae0e19e1d476eb5
sun0H <108638572+sun0H@users.noreply.github.com>
2023-04-20 10:14:30 +0900

    틱택토 EventBus

---
+++ 7.틱택토EventBus/TableComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <table>
+    <tr-component v-for="(rowData, index) in tableData" :key="index" :row-data="rowData" :row-index="index"></tr-component>
+  </table>
+</template>
+
+<script>
+import TrComponent from "./TrComponent.vue";
+export default {
+  name: 'TableComponent',
+  props: {
+    tableData:Array,
+  },
+  components: {TrComponent},
+  computed: {
+
+  },
+}
+
+</script>
+
+<style>
+
+</style>
